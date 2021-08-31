53c737e13e4efd5dbf1e79450699e950bf78d7d6
jon <jonprosser84@gmail.com>
2021-08-31 20:38:50 +0100

    Max react refresher complete up to routing

---
+++ src/components/Modal.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Modal = (props) => {
+  return (
+    <div className="modal">
+      <p>Are you sure?</p>
+      <button className="btn btn--alt" onClick={props.onClick}>Cancel</button>
+      <button className="btn" onClick={props.onClick}>Confirm</button>
+
+    </div>
+  )
+}
+
+export default Modal
